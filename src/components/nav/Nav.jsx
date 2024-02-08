0c6001cb7ef529fbedcf90377ec1d81765ea1b91
SarayAnta <s.rodriguezanta@gmail.com>
2024-02-08 11:53:29 +0100

    add button to the navbar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './nav.css'
+import Addbutton from '../button/Button-add/add-button'
 
 const Nav = () => {
   return (
     <>
     <nav>
       <h1>MUSEO DE BICICLETAS</h1>
+      <Addbutton/>
     </nav>
     </>
   )
