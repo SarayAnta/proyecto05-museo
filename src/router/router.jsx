f8316750f5a8b370ffca88ae23dd79aef9560753
SarayAnta <s.rodriguezanta@gmail.com>
2024-02-08 12:11:36 +0100

    create home.css

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../components/Home";
+import Home from "../components/home/Home";
 import Contacto from "../components/Contacto";
 
 const router = createBrowserRouter([
