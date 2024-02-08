4eb87231174bb29d28b78bf221a61b2c57a570c9
elenalope <elopmorales97@gmail.com>
2024-02-08 14:07:47 +0100

    including navbar in newitem

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,20 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../components/Home";
-import Addbutton from "../components/button/Button-add/add-button";
-import Contacto from "../components/Contacto";
 import NewItem from "../components/newitem/NewItem";
+
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <Home/>,
     },
-
-    {
-        path: "/contacto",
-        element: <Contacto/>,
-    },
-
     {
       path: "/NewItem",
       element: <NewItem/>,
