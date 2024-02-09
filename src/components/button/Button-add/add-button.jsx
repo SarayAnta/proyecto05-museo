8c110427a74b04dda4ff5816adac2caecd5d4585
TrianaSolerMartin <triana287@gmail.com>
2024-02-09 12:38:01 +0100

    i dont know

--- src/components/button/Button-add/add-button.jsx
+++ src/components/button/Button-add/add-button.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 import './add-button.css'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
  
 const Addbutton = () => {
     const navigate = useNavigate();
