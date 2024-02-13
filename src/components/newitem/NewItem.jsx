925b3c5981ce12d9c6704cbea5bff915bc63631a
elenalope <elopmorales97@gmail.com>
2024-02-13 15:42:14 +0100

    styled add form

--- src/components/newitem/NewItem.jsx
+++ src/components/newitem/NewItem.jsx
@@ -6,31 +6,35 @@ import Footer from '../footer/Footer';
 
 const NewItem = () => {
 
-    const { register, formState: { errors }, handleSubmit } = useForm();
+    const { register, formState: { errors }, handleSubmit, reset} = useForm();
 
     const onSubmit = (data) => {
         console.log(data);
+        // Mostrar mensaje de éxito
+        alert('¡Tu bicicleta fue añadida correctamente!');
+        // Reiniciar el formulario
+        reset();
     }
-
-    return(
-    <>
-      <Nav/>      
+        
+    return <>
+        <Nav/>  
         <form onSubmit={handleSubmit(onSubmit)}>
             <div>
                 <label>Modelo</label>
                 <input className='model' type="text" {...register('modelo', {
                     required: true,
-                    minLength: 5
                 })}/>
                 {errors.modelo?.type === 'required' && <p>El campo modelo es requerido</p>}
-                {errors.modelo?.type === 'minLength' && <p>El campo modelo debe tener más de 5 caracteres</p>}
             </div>
             <div>
                 <label>Velocidades</label>
-                <input className='speeding' type="text" {...register('velocidades')}/>
+                <input className='speeding' type="text" {...register('velocidades', {
+                    required: true,
+                })}/>
+                {errors.velocidades?.type === 'required' && <p>El campo velocidades es requerido</p>}
             </div>
             <div className='cuadred'>
-                <div className='frame cuadred'>
+                <div className='frame'>
                     <label>Cuadro</label>
                     <select {...register('cuadro')}>
                         <option value="al">Aluminio</option>
@@ -39,22 +43,24 @@ const NewItem = () => {
                         <option value="ot">Otros</option>
                     </select>
                 </div>
-                <div className='electric cuadred'>
+                <div className='electric'>
                     <label>Eléctrica</label>
                     <input className="checkbox-css" type="checkbox" {...register('eléctrica')} />
                 </div>
             </div>
             <div>
                 <label htmlFor="imageUpload">Img URL</label>
-                <input
-                type="file"
-                accept="image/*"
-                />
+                <input type="text" {...register('imageUpload', {
+                pattern: /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/,
+                required:true,
+                })}/>
+                {errors.imageUpload?.type === 'pattern' && <p>El formato de la url de la imagen es incorrecto</p>}
+                {errors.imageUpload?.type === 'required' && <p>El campo url de la imagen es requerido</p>}
             </div>
-            <input type="submit" value="Añadir" className='add'/>
+            <input type="submit" value="Añadir"/>
         </form>
-    <Footer/>
+        <Footer/>
     </>
-)}
-
+            }
+           
 export default NewItem;
