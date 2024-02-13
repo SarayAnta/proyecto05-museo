7d9cf566fb64c99a2ba9f08c0838a716d0e5046c
jessica noguera <jnoguera.fact@gmail.com>
2024-02-13 14:03:16 +0100

    style

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,29 +1,54 @@
 import React from 'react';
-import "./footer.css";
+import { useNavigate } from "react-router-dom";
+import styled from 'styled-components';
 
 import Copyright from '../../assets/img/Copyright.png';
 import mail from '../../assets/img/mail.png';
 import GitHub from '../../assets/img/GitHub.png';
-import Instagram from '../../assets/img/Instagram.png'
+import Instagram from '../../assets/img/Instagram.png';
 import Twitter from '../../assets/img/TwitterX.png';
+
+// Cambiado "Footer" por "footer" para coincidir con el nombre de la etiqueta HTML
+const FooterContainer = styled.footer`
+  background-color: #393939;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: -1vh;
+  
+  img {
+    margin: 0 10px;
+    width: 50px;
+    height: 50px;
+  }
+
+  /* Estilos para hacer el footer responsive */
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+    padding: 10px;
+  }
+`;
 
 const Footer = () => {
   return (
-    <div className="footer-container">
-    <img src={Copyright} alt="Copyright" />
-    <a href="https://gmail.com/">
-        <img src={mail} alt="mail" />
-      </a>
-    <a href="https://github.com/">
-        <img src={GitHub} alt="GitHub" />
-      </a>
-    <a href="https://www.instagram.com/">
-        <img src={Instagram} alt="Instagram" />
-      </a>
-      <a href="https://twitter.com/">
-        <img src={Twitter} alt="Twitter" />
-      </a>
-    </div>
+    <FooterContainer>
+      <div className="footer-container">
+        <img src={Copyright} alt="Copyright" />
+        <a href="https://gmail.com/">
+          <img src={mail} alt="mail" />
+        </a>
+        <a href="https://github.com/">
+          <img src={GitHub} alt="GitHub" />
+        </a>
+        <a href="https://www.instagram.com/">
+          <img src={Instagram} alt="Instagram" />
+        </a>
+        <a href="https://twitter.com/">
+          <img src={Twitter} alt="Twitter" />
+        </a>
+      </div>
+    </FooterContainer>
   );
 }
 
