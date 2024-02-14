7d1ec02ff487621490dd88e77e95aa71f8886759
elenalope <elopmorales97@gmail.com>
2024-02-14 14:03:14 +0100

    método POST mediante service.js

--- src/services/service.js
+++ src/services/service.js
@@ -13,3 +13,26 @@ export    const deleteBicycle = async (id) => {
     method: 'DELETE'});
     return response;
 };
+
+//Método POST
+
+export const postBicycle = async (data) => {
+  try {
+      const response = await fetch('http://localhost:3000/bicycles', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+      });
+
+      if (response.ok) {
+          return { success: true };
+      } else {
+          return { success: false, error: 'Hubo un problema al añadir tu bicicleta. Por favor, intenta de nuevo más tarde.' };
+      }
+  } catch (error) {
+      console.error('Error:', error);
+      return { success: false, error: 'Hubo un problema al añadir tu bicicleta. Por favor, intenta de nuevo más tarde.' };
+  }
+};
