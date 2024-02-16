078c1d62f3c70d802e32503bbd7d94933a38848b
TrianaSolerMartin <triana287@gmail.com>
2024-02-16 13:19:11 +0100

    tryin to load data into edit form

--- src/components/edit/Edit.jsx
+++ src/components/edit/Edit.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import styled from 'styled-components';
-import { updateItem } from '../../services/service';
+import { updateItem, getItemById } from '../../services/service';
 import {useParams} from 'react-router';
 
 const StyledEdit = styled.div`
