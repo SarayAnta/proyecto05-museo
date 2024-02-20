fce46fc9c4636254bb7afa4dff1307bd6c74a388
TrianaSolerMartin <triana287@gmail.com>
2024-02-20 15:01:12 +0100

    tryingcloud

--- src/components/ImageUpload/imgupload.jsx
+++ src/components/ImageUpload/imgupload.jsx
@@ -1,44 +1,17 @@
 import React, { useState } from "react";
-import { Container, FormGroup, Input } from "reactstrap";
+import axios from "axios"
 
-const ImageUpload = (props) => {
-    const [image, setImage] = useState("");
-    const [loading, setLoading] = useState(false);
+function UploadImage () ´{
+    const [Url_Image, setUrl_Image] = useState("");
+    const changeUploadImage
 
-    const upLoadImage = async (e) => {
-        const files = e.target.files;
-        const data = new FormData();
-        data.append("file", files[0]);
-        // Establecer el nombre de la carpeta en Cloudinary
-        data.append("upload_bicycle", "bicycles-museum")
-        setLoading(true);
-        const res = await fetch("https://api.cloudinary.com/v1_1/dlg7gpmha/bicycles-museum/upload", {
-            method: "POST",
-            body: data,
-        });
-        const file = await res.json();
-        setImage(file.secure_url);
-        setLoading(false);
-    }
-
-    return (
-        <> 
-            <Container>
-                <h1>SUBIENDO IMÁGENES</h1>
-                <FormGroup>
-                    <Input type="file" name="file" id="exampleFile" placeholder='Sube tu imagen aquí' onChange={upLoadImage} />
-                    <label htmlFor="exampleFile">Selecciona una imagen</label>
-                </FormGroup>
-                {image && (
-                    <div>
-                        <h2>Imagen cargada:</h2>
-                        <img src={image} alt="Uploaded" />
-                    </div>
-                )}
-                {loading && <p>Cargando...</p>}
-            </Container>
-        </>
-    );
+    return <>
+    <h1>Seleccionar imagen para cloudinary</h1>
+    <div>
+        <input type="file" accept="image/*" />
+        <div>
+            <img src={Url_Image} alt="" />
+        </div>
+    </div>
+    </>
 }
-
-export default ImageUpload;
