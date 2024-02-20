59d14fba1f4c8950d069138f5a10b051e690862b
TrianaSolerMartin <triana287@gmail.com>
2024-02-20 15:02:10 +0100

    a

--- src/components/ImageUpload/imgupload.jsx
+++ src/components/ImageUpload/imgupload.jsx
@@ -1,17 +1,42 @@
+import axios from "axios";
 import React, { useState } from "react";
-import axios from "axios"
 
-function UploadImage () ´{
+function UploadImage() {
     const [Url_Image, setUrl_Image] = useState("");
-    const changeUploadImage
 
-    return <>
-    <h1>Seleccionar imagen para cloudinary</h1>
-    <div>
-        <input type="file" accept="image/*" />
-        <div>
-            <img src={Url_Image} alt="" />
-        </div>
-    </div>
-    </>
+    const changeUploadImage = async (e) => {
+        const file = e.target.files[0];
+        const data = new FormData(); // Corregido el nombre de FormData
+
+        data.append("file", file);
+        data.append("upload_preset", "Presents_react");
+
+        try {
+            const response = await axios.post(
+                "http://api.cloudinary.com/v1_1/dlg7gpmha/image/upload",
+                data
+            );
+            console.log(response.data);
+            setUrl_Image(response.data.secure_url);
+        } catch (error) {
+            console.error("Error al cargar la imagen:", error);
+        }
+    };
+
+    return (
+        <>
+            <h1>Seleccionar imagen para Cloudinary</h1>
+            <div>
+                <input type="file" accept="image/*" onChange={changeUploadImage} />
+                {Url_Image && (
+                    <div>
+                        <img src={Url_Image} alt="Uploaded" />
+                        <button>Eliminar imagen</button>
+                    </div>
+                )}
+            </div>
+        </>
+    );
 }
+
+export default UploadImage;
