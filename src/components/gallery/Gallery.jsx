e40f5ab28a9e6a073e20d3c0dceed7c790920089
jessica noguera <jnoguera.fact@gmail.com>
2024-02-26 10:18:06 +0100

    cambios

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,49 +1,141 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import BackgroundHome from '../../assets/img/BackgroundHome.png';
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
-import Gallery1 from '../../assets/img/Gallery1.png';
+import { getBicycles, deleteBicycle } from '../../services/service';
 
-// Cambiado "gallery" por "div" para usar la clase de estilos correctamente
-const GalleryContainer = styled.div`
+const HomeContainer = styled.div`
+
+body {
+    margin: 0;
+  }
+
+
+  .title-gallery {
+    text-align: center;
+    font-family: 'Jost', sans-serif;
+    font-size:125%;
+    text-shadow:  4px 4px 4px #D9D9D9;
+    text-transform: uppercase;
+    
+  }
+  
+
+  .background-img {
+    width: 100%;
+    height: auto;
+  }
+
+  .gallery {
+    display: flex; 
+    flex-wrap: wrap; 
+    justify-content: space-around; 
+  }
+
   .gallerygrid {
-    display: flex;
-    justify-content: space-around;
-    padding: auto;
+    margin: 10px; 
+    text-align: center; 
   }
 
-  .galleryimg {
-    width: 15vw;
-    height: 20vh;
-    padding: 5vh 5vw;
-   
+  .bicyclesimg {
+    max-width: 17vw;
+    max-height: 45vh; 
+    border: 0.5rem solid #D9D9D9;
+    cursor: pointer;
   }
 
-  /*
-  @media only screen and (max-width: 600px) {
-    .gallerygrid {
-      flex-direction: column; 
-      align-items: center;
-    }
+  .bicyclesimg:hover {
+    transform: scale(1.1);
+    transition: 0.5s;
 
-    .galleryimg {
-      width: 40vw;
-      height: 20vh; 
-      padding: 5vh 0; 
-      margin-bottom: 20px; 
-    }
-  }*/
+  }
+
+  p {
+    text-align: center;
+    font-family: 'Jost', sans-serif;
+    font-size: 100%;
+    text-transform: uppercase;
+    text-decoration: bold;
+    text-shadow:  4px 4px 4px #D9D9D9;
+    padding-top:5%;
+
+  }
+
+ gallery-button {
+  
+  display: flex;
+  width: 5vw;
+  justify-content: space-around;
+  
+  
+ }
+
+ button img {
+  width: 50%;
+  height: auto;
+  
+
+ }
+
+button.edit-button, button.delete-button  {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  margin: 0.75rem;
+  transition: 0.5s;
+  
+
+}
+
+
+
+button.edit-button:hover, button.delete-button:hover {
+  transform: scale(1.5);
+}
+
+
 `;
 
-const Gallery = () => { // Cambiado "props" por "id" para que coincida con el nombre que le damos en el archivo App.jsx
-  return (
+const Home = () => { // Crea un componente Home
+  const [bicycles, setBicycles] = useState([]); // Declara una constante bicycles y una función setBicycles que almacenan un array vacío UseState, se desestructura el array en dos elementos
+  const navigate = useNavigate(); // Declara una constante navigate que almacena el hook useNavigate
+
+  useEffect(() => {  // Crea un efecto que se ejecuta al renderizar el componente
+      const fetchData = async () => {  // Crea una función fetchData que se ejecuta de manera asíncrona
+      const data = await getBicycles() // Declara una constante data que almacena el resultado de la función getBicycles
+      console.log(data); // Muestra en consola el contenido de la constante data
+      setBicycles(data); // Ejecuta la función setBicycles con el contenido de la constante data como argumento
+    }
+    fetchData(); // Ejecuta la función fetchData
+    }
+
+   , []);
+
+
+  return ( // Retorna un fragmento con el contenido del componente
     <>
-      {/* Cambiado "Gallery" por "GalleryContainer" */}
-      <GalleryContainer className='gallerygrid'>
-        <img src={Gallery1} className="galleryimg" alt="Imagen de fondo de una chica apoyada sobre una bicicleta azul"/>
-        <img src={Gallery1} className="galleryimg" alt="Imagen de fondo de una chica apoyada sobre una bicicleta azul"/>
-      </GalleryContainer>
+    <HomeContainer>
+        <img className="background-img"src={BackgroundHome} alt="Imagen de fondo de una chica apoyada sobre una bicicleta azul" />
+        <h2 className="title-gallery">Modelos de bicicletas</h2>
+        <div className='gallery'>
+          {bicycles.map((bicycle) => (
+            <div className='gallerygrid' key={bicycle.id}>
+              <img  onClick={() => navigate(`/card/${bicycle.id}`)} className="bicyclesimg" src={bicycle.image} alt={bicycle.model} />
+              <p>{bicycle.model}</p>
+              <div className="gallery-button" >
+              <button className="edit-button" onClick={() => navigate(`/Edit/${bicycle.id}`)}>
+                <img src="src\assets\img\Edit.png" alt="" />
+              </button>
+              <button className="delete-button" onClick={() => {deleteBicycle(`${bicycle.id}`); navigate(0)}}>
+                <img src="src\assets\img\Delete.png"></img></button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </HomeContainer>
     </>
   );
 }
+//useparamt luego peticion get de id que tngo en la url 
+export default Home;
 
-export default Gallery;
