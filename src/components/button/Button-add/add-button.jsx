385be3e2ceb7c12b4a2b903cd57d44001d892a8d
Fátima Cosme Salvador <146832705+pointfs@users.noreply.github.com>
2024-02-28 10:11:16 +0100

    changesadd

--- src/components/button/Button-add/add-button.jsx
+++ src/components/button/Button-add/add-button.jsx
@@ -3,64 +3,66 @@ import { useNavigate } from "react-router-dom";  // Importa el hook useNavigate
 import styled from "styled-components";  // Importa styled-components
  
 const AddButton = styled.button`    // Crea un componente AddButton con styled-components
-        background-color: #3DE361;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-family: 'Jost', sans-serif;
-        width: 25vw;
-        height: 8vh;
-        max-width: 150px;
-        margin-right: 20px;
-        border-radius: 15px;
+background-color: #3DE161d2;
+display: flex;
+color: #FFFFFF;
+justify-content: center;
+align-items: center;
+font-family: 'Jost', sans-serif;
+width: 25vw;
+height: 8vh;
+max-width: 150px;
+margin-right: 20px;
+border-radius: 15px;
+font-weight: bold;
+
+
+&:hover {
+    background-color: #FFFFF;
+    color: black;
+    transform: scale(1.1);
+    transition: 0.5s;
+}
+
+&:active {
+    background-color: #3DE161d2;
+    color: black;
+    transform: scale(0.9);
+    transition: 0.5s;
+}
+
+
+.add-bicycle {
+    justify-content: center;
+    font-size:1vw;
+    margin:0;
+}
+
+//media queries
+@media (width > 550px) {
+    width: 30%;
+    margin-right: 10px;
+
+    add-bicycle {
+        font-size: 50%;
+        text-align: center;
         
 
-        &:hover {
-            background-color: #3DE361;
-            color: black;
-            transform: scale(1.1);
-            transition: 0.5s;
-        }
+}
 
-        &:active {
-            background-color: #3DE361;
-            color: black;
-            transform: scale(0.9);
-            transition: 0.5s;
-        }
 
-    
-        .add-bicycle {
-            justify-content: center;
-            font-size:1vw;
-            margin:0;
-        }
 
-        //media queries
-        @media (width > 550px) {
-            width: 30%;
-            margin-right: 10px;
 
-            add-bicycle {
-                font-size: 50%;
-                text-align: center;
-                
-
-        }
-
-      
-
-       
-    `;
+`;
 
 const Addbutton = () => {   // Crea un componente Addbutton
-    const navigate = useNavigate();  // Declara una constante navigate que almacena el hook useNavigate
+const navigate = useNavigate();  // Declara una constante navigate que almacena el hook useNavigate
 
-    return (  // Devuelve el componente AddButton con el evento onClick que redirige a la ruta /NewItem// Añade la clase "add-button" al componente AddButton
-        <AddButton className="add-button" onClick={() => navigate("/NewItem")}>             
-                <p className="add-bicycle">Añadir bicicleta</p>
-        </AddButton>
-        );
+return (  // Devuelve el componente AddButton con el evento onClick que redirige a la ruta /NewItem// Añade la clase "add-button" al componente AddButton
+<AddButton className="add-button" onClick={() => navigate("/NewItem")}>             
+        <p className="add-bicycle">AÑADIR BICICLETA</p>
+</AddButton>
+);
 }
 
 export default Addbutton; // Exporta el componente Addbutton
